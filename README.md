5c28e69de2128d1b009bccae7c863f3b955d1b13
pmg29 <phw2whp@gmail.com>
2025-09-26 13:30:01 +0900

    Add additional script an README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Docker 프로젝트 설정 가이드
+
+이 프로젝트는 Docker 컨테이너를 사용하여 여러 서비스를 배포합니다.
+
+## 1단계: 프로젝트 클론 
+```bash 
+git clone [https://github.com/choma45/docker.git](https://github.com/choma45/docker.git)
+cd docker
+
+## 2단계: 환경 설정 스크립트 실행
+sudo sh install.sh
+
+## 3단계: 서비스 실행
+docker-compose up -d
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Centos Stream 9
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo usermod -aG docker $USER 
+
+echo "================================================"
+echo "Docker 및 관련 패키지 설치가 완료되었습니다.       "
+ehco "재로그인 후 설정이 완료됩니다.                    "
+echo "================================================"
+
